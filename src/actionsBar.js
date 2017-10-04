2ce469a057225e6ab734c023046cd0df48b7f5e4
Daria <ivanovich.daria.olegovna@gmail.com>
2017-10-04 17:27:51 +0300

    change package and fix action bar items

--- src/actionsBar.js
+++ src/actionsBar.js
@@ -1,75 +1,33 @@
 import React, { Component } from 'react';
+import { Animated } from 'react-native';
 
-import Icon from 'react-native-vector-icons/MaterialIcons';
-
-import {
-    TouchableOpacity,
-    Animated,
-    View
-} from 'react-native';
+import ActionsBarItem from './actionsBarItem';
 
 import styles from './styles';
 
-export default class EditablieFlatListItem extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: this.props.id,
-            selected: this.props.selected // turn on long tap
-        };
-
-        this._onPress = this._onPress.bind(this);
-    }
-
-    componentDidMount() {
-        this.props.onRef(this);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            selected: nextProps.selected
-        });
-    }
-
-    _onPress() {
-        this.setState({
-            selected: !this.state.selected
-        });
-
-        return false;
-    }
+class ActionsBar extends Component {
 
     render() {
         return (
-            <TouchableOpacity
-                onLongPress = { this.props.onLongPress }
-                onPress = { this.props.editingMode ? this.props.onPress.bind(this.state.id) : this.props.onRowClick }>
-                <View style = { styles.itemRow }>
+            <Animated.View
+                style = { [ styles.actionBar,
+                    { height: this.props.topActionBarAnimation }
+                ] }>
 
-                    <Animated.View
-                        style = { [
-                            {
-                                width: this.props.iconSize,
-                                height: this.props.iconSize,
-                                marginLeft: this.props.editingMode ? 16 : 0
-                            },
-                            this.props.iconStyles,
-                            styles.itemIconContainer,
-                            this.state.selected ? styles.itemIconContainerChecked : null
-                        ] }>
-                        <Icon
-                            name = { 'done' }
-                            style = { styles.itemIcon } />
-                    </Animated.View>
-
-                    <View style = { styles.itemRowTextContainer }>
-                        {this.props.renderRow}
-                    </View>
-
-                </View>
-            </TouchableOpacity>
+                <ActionsBarItem
+                    iconName = { 'done-all' }
+                    onPress = { this.props.selectAll }
+                    text = { 'Select All' } />
+                <ActionsBarItem
+                    iconName = { 'delete' }
+                    onPress = { this.props.remove }
+                    text = { 'Remove' } />
+                <ActionsBarItem
+                    style = {{ fontSize: 15 }}
+                    onPress = { this.props.cancel }
+                    text = { 'Cancel' } />
+            </Animated.View>
         );
     }
 }
-
+export default ActionsBar;
